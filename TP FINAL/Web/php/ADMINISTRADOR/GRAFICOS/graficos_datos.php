db45b35ff9056a34e9c7a7cde92d80735be824bb
Maxipadula <padula.maximiliano@gmail.com>
2015-11-24 13:26:45 -0300

    graficos

--- TP FINAL/Web/php/ADMINISTRADOR/GRAFICOS/graficos_datos.php
+++ TP FINAL/Web/php/ADMINISTRADOR/GRAFICOS/graficos_datos.php
@@ -29,18 +29,46 @@
            		
 
 		  
-           <li><a href="<?php echo $rendimiento_km ?>">RENDIMIENTO DE LOS VEHICULOS EN KM</a></li>
-		
-		   <li><a href="<?php echo $rendimiento_combustible ?>">RENDIMIENTO PROMEDIO DE CONSUMO DE COMBUSTIBLE</a></li>
 
-		   <li> <a href="../<?php echo $administrador_home2 ?>">SALIR</a></li>
+
+		   <li> <a href="../../<?php echo $administrador_home ?>">SALIR</a></li>
 	
 
        </ul>
  
 	</nav>
+	</div>
+	</div>
 	<div id="divContenedor">
-
+		    <div class="divTabla">
+		<?php
+			$consulta_transporte = mysql_query ("SELECT M.descripcion marca,MO.descripcion modelo, T.patente patente,T.id_transporte ID,E.descripcion estado,T.km_recorridos km,T.num_chasis chasis,T.num_motor motor
+											 FROM estado E inner join
+												  transporte T on E.id_estado = T.id_estado inner join 
+											      vehiculo V on T.id_vehiculo = V.id_vehiculo inner join 
+											      marca M on V.id_marca = M.id_marca inner join 
+											      modelo MO on V.id_modelo = MO.id_modelo")or die (mysql_error());
+		
+			echo "<form class='chequeado' method='post' action=". $validar_modificacion_transportes.">";
+			if ($row = mysql_fetch_array($consulta_transporte)){
+				echo "<table border = '1'> \n";
+				echo "<tr><td>MARCA</td><td>MODELO</td><td>PATENTE</td><td>ESTADO</td><td>KM RECORRIDOS</td><td>NUMERO DE CHASIS</td><td>NUMERO DE MOTOR</td><td>GRAFICAR</td><td>GRAFICAR</td></tr>\n";
+			
+				do{
+					echo "<tr><td>".$row["marca"]."</td><td>".$row["modelo"]."</td><td>".$row["patente"]."</td><td>".$row["estado"]."</td><td>".$row["km"].".KM</td><td>".$row["chasis"]."</td><td>".$row["motor"]."</td><td class='tBotonAgregar'><a target='_blank' href='".$graficar_km ."?ID=".$row["ID"]."' class = 'tLink'>RENDIMIENTO EN KM</a></td><br>
+					       <td class='tBotonAgregar'><a target='_blank' href='".$graficar_combustible ."?ID=".$row["ID"]."' class = 'tLink'>CONSUMO DE COMBUSTIBLE</a></td><br></tr> \n";     
+				
+				}while ($row = mysql_fetch_array($consulta_transporte));
+			
+				echo "</table> \n";
+			 
+			
+			} else {
+				echo "<h3> No se encontraron registros </h3>";
+		} 
+		
+		?>	
+		</div>
 
 		</div>
 </body>
